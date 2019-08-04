c4ae949bec02fd04c374fa651b353f46a3e6e7fe
Ben Lin <522181941@qq.com>
2019-08-04 17:00:20 +0800

    plot scripts

--- data/CAD_Model/plot_optimal_neurons_num.m
+++ data/CAD_Model/plot_optimal_neurons_num.m
@@ -8,7 +8,7 @@
 %   output_mat_1 - target data.
 
 x = input_mat;
-t = output_mat_1;
+t = output_mat(5,:);
 
 % Choose a Training Function
 % For a list of all training functions type: help nntrain
@@ -17,81 +17,80 @@ t = output_mat_1;
 % 'trainscg' uses less memory. Suitable in low memory situations.
 trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
 
-% Create a Fitting Network
-hiddenLayerSize = 10;
-net = fitnet(hiddenLayerSize,trainFcn);
+train_perform_list = [];
+validation_perform_list = [];
+test_perform_list = [];
+tr_stop_list = {};
+for i=1:20
+    % Create a Fitting Network
+    hiddenLayerSize = i;
+    net = fitnet(hiddenLayerSize,trainFcn);
 
-% Choose Input and Output Pre/Post-Processing Functions
-% For a list of all processing functions type: help nnprocess
-net.input.processFcns = {'removeconstantrows','mapminmax'};
-net.output.processFcns = {'removeconstantrows','mapminmax'};
+    % Choose Input and Output Pre/Post-Processing Functions
+    % For a list of all processing functions type: help nnprocess
+    net.input.processFcns = {'removeconstantrows','mapminmax'};
+    net.output.processFcns = {'removeconstantrows','mapminmax'};
 
-% Setup Division of Data for Training, Validation, Testing
-% For a list of all data division functions type: help nndivision
-net.divideFcn = 'dividerand';  % Divide data randomly
-net.divideMode = 'sample';  % Divide up every sample
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+    % Setup Division of Data for Training, Validation, Testing
+    % For a list of all data division functions type: help nndivision
+    net.divideFcn = 'dividerand';  % Divide data randomly
+    net.divideMode = 'sample';  % Divide up every sample
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
 
-% Choose a Performance Function
-% For a list of all performance functions type: help nnperformance
-net.performFcn = 'mse';  % Mean Squared Error
+    % Choose a Performance Function
+    % For a list of all performance functions type: help nnperformance
+    net.performFcn = 'mse';  % Mean Squared Error
 
-% Choose Plot Functions
-% For a list of all plot functions type: help nnplot
-net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
-    'plotregression'};
+    % Choose Plot Functions
+    % For a list of all plot functions type: help nnplot
+    net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
+        'plotregression'};
 
-% Train the Network
- net.trainParam.goal = 0.001;
-net.trainParam.showWindow = false;
-net.trainParam.showCommandLine = true;
-[net,tr] = train(net,x,t);
+    % Train the Network
+     net.trainParam.goal = 1e-10;
+    net.trainParam.showWindow = false;
+    net.trainParam.showCommandLine = true;
+    net.trainParam.max_fail = 4;
+    [net,tr] = train(net,x,t);
 
-% Test the Network
-y = net(x);
-e = gsubtract(t,y);
-performance = perform(net,t,y)
+    % Test the Network
+    y = net(x);
+    e = gsubtract(t,y);
+    performance = perform(net,t,y)
 
-% Recalculate Training, Validation and Test Performance
-trainTargets = t .* tr.trainMask{1};
-valTargets = t .* tr.valMask{1};
-testTargets = t .* tr.testMask{1};
-trainPerformance = perform(net,trainTargets,y)
-valPerformance = perform(net,valTargets,y)
-testPerformance = perform(net,testTargets,y)
-
-% View the Network
-%view(net)
-
-% Plots
-% Uncomment these lines to enable various plots.
-%figure, plotperform(tr)
-%figure, plottrainstate(tr)
-%figure, ploterrhist(e)
-%figure, plotregression(t,y)
-%figure, plotfit(net,x,t)
-
-% Deployment
-% Change the (false) values to (true) to enable the following code blocks.
-% See the help for each generation function for more information.
-if (false)
-    % Generate MATLAB function for neural network for application
-    % deployment in MATLAB scripts or with MATLAB Compiler and Builder
-    % tools, or simply to examine the calculations your trained neural
-    % network performs.
-    genFunction(net,'myNeuralNetworkFunction');
-    y = myNeuralNetworkFunction(x);
+    % Recalculate Training, Validation and Test Performance
+    trainTargets = t .* tr.trainMask{1};
+    valTargets = t .* tr.valMask{1};
+    testTargets = t .* tr.testMask{1};
+    trainPerformance = perform(net,trainTargets,y)
+    train_perform_list = [train_perform_list, trainPerformance];
+    valPerformance = perform(net,valTargets,y)
+    validation_perform_list = [validation_perform_list,valPerformance];
+    testPerformance = perform(net,testTargets,y)
+    test_perform_list = [test_perform_list, testPerformance];
+    tr_stop_list{end+1} = tr.stop; 
 end
-if (false)
-    % Generate a matrix-only MATLAB function for neural network code
-    % generation with MATLAB Coder tools.
-    genFunction(net,'myNeuralNetworkFunction','MatrixOnly','yes');
-    y = myNeuralNetworkFunction(x);
-end
-if (false)
-    % Generate a Simulink diagram for simulation or deployment with.
-    % Simulink Coder tools.
-    gensim(net);
-end
+
+
+%%
+
+figure
+hold on
+y_lim_max = max(test_perform_list)*1.2;
+plot(1:size(test_perform_list,2), test_perform_list,'-ok',...
+'LineWidth',2,...
+'MarkerFaceColor','b',...
+'MarkerSize',10)
+
+set(gca,'FontSize',20)
+xlabel('Number of neurons');
+ylabel('E_{RMS}');
+ylim([0,y_lim_max])
+
+
+optimal_neuron_index = 9;
+plot([optimal_neuron_index,optimal_neuron_index],[0, y_lim_max],'--k','LineWidth',2)
+hold off
+
